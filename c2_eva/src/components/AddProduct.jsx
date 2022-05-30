cec1d8a3157f71b5637b1950d9585e95b3a0e073
karthick-pandi <93534549+karthick-pandi@users.noreply.github.com>
2022-05-30 14:45:37 +0530

    ab

--- c2_eva/src/components/AddProduct.jsx
+++ c2_eva/src/components/AddProduct.jsx
@@ -12,14 +12,14 @@ const AddProduct = () => {
 
   return (
     <>
-      <Button my={4} data-cy="add-product-button"></Button>
+      <Button my={4} data-cy="add-product-button">Add product</Button>
       <Modal>
         <ModalBody pb={6}>
           <Input data-cy="add-product-title" />
           <Select data-cy="add-product-category">
-            <option data-cy="add-product-category-shirt"></option>
-            <option data-cy="add-product-category-pant"></option>
-            <option data-cy="add-product-category-jeans"></option>
+            <option data-cy="add-product-category-shirt">shirt</option>
+            <option data-cy="add-product-category-pant">pant</option>
+            <option data-cy="add-product-category-jeans">jeans</option>
           </Select>
           <RadioGroup data-cy="add-product-gender">
             <Radio data-cy="add-product-gender-male"></Radio>
--- c2_eva/src/components/Pagination.jsx
+++ c2_eva/src/components/Pagination.jsx
@@ -11,9 +11,9 @@ const Pagination = () => {
       <Button data-cy="pagination-first-button"></Button>
       <Button data-cy="pagination-previous-button"></Button>
       <Select data-cy="pagination-limit-select">
-        <option data-cy="pagination-limit-3"></option>
-        <option data-cy="pagination-limit-6"></option>
-        <option data-cy="pagination-limit-9"></option>
+        <option data-cy="pagination-limit-3">3</option>
+        <option data-cy="pagination-limit-6">6</option>
+        <option data-cy="pagination-limit-9">9</option>
       </Select>
       <Button data-cy="pagination-next-button"></Button>
       <Button data-cy="pagination-last-button"></Button>
